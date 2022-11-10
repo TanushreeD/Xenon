b93808ae7a1049b5c7434ffa000de92caf5f5615
TanushreeD <tanushree5.dwivedi@gmail.com>
2022-11-10 07:39:09 +0530

    xenon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { UserProvider } from './components/UseContext';
+import { useState } from "react";
+import Login from "./components/Login";
+import ContactUs from "./components/ContactUs";
+import SignUp from "./components/SignUp";
+import Home from "./components/Home";
+import Header from "./components/Header";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(sessionStorage.getItem("user"))
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grey-card-container">
+      <div>
+        <UserProvider user={currentUser}>
+          <BrowserRouter>
+            <Header/>
+            <Routes>
+              <Route element={<Home></Home>} path="/"/>
+              <Route element={<SignUp></SignUp> }path="signup"/>
+
+              <Route element={<Login></Login>} path="login" />
+              <Route element={<ContactUs></ContactUs>} path="contact" />
+            </Routes>
+          </BrowserRouter>
+        </UserProvider>
+      </div>
     </div>
   );
 }
